4c49166dc7334a0f903f5e851234d60982f8123e
Thembelani <139681992+ThembelaniProject@users.noreply.github.com>
2026-06-05 13:58:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("bookBtn");
+
+button.addEventListener("click", () => {
+
+    alert(
+        "Bookings are now open for DJ Mthizanation 2026!"
+    );
+
+});
